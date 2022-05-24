a32ee58b804d5b7baec12c2a344b5320c948e4db
PavanAnugu <103167623+PavanAnugu@users.noreply.github.com>
2022-05-24 12:48:56 +0530

    Add files via upload

---
+++ assignment/StringToLowerCase.java
@@ -0,0 +1,13 @@
+public class Exercise {
+       public static void main(String[] args)
+    {
+        String str = "The Quick BroWn FoX!";
+
+        // Convert the above string to all lowercase.
+        String lowerStr = str.toLowerCase();
+
+        // Display the two strings for comparison.
+        System.out.println("Original String: " + str);
+        System.out.println("String in lowercase: " + lowerStr);
+    }
+}
